dc158cdc83532d087799299c0c264dcb3340e9f5
devferx <ferkki@hotmail.com>
2025-01-09 16:51:32 -0400

    Add pagination functionality to Pokémon data fetching service

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-import type { GetPokemonsResponse } from '@/interfaces/get-pokemons-response'
-import { Pokemon } from '@/models/pokemon'
 import { adaptRawPokemon } from '@/adapters/adapt-raw-pokemon'
+
+import type { GetPokemonsResponse } from '@/interfaces/get-pokemons-response'
+import type { Pokemon } from '@/models/pokemon'
 
 const pokeApi = axios.create({
   baseURL: 'https://pokeapi.co/api/v2',
@@ -17,5 +18,16 @@ export const getPokemons = async (
   })
 
   const pokemons = adaptRawPokemon(data.results)
+  return pokemons
+}
+
+export const getPokemonByPage = async (
+  page: number,
+  itemsPerPage = 30,
+): Promise<Pokemon[]> => {
+  const limit = itemsPerPage
+  const offset = (page - 1) * limit
+
+  const pokemons = await getPokemons(limit, offset)
   return pokemons
 }
