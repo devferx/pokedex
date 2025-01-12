2db96733f553410658837505795888194d3cbf57
devferx <ferkki@hotmail.com>
2025-01-11 23:31:20 -0400

    Update Badge component to use w-fit class for improved styling

--- src/domains/single-pokemon/components/badge.tsx
+++ src/domains/single-pokemon/components/badge.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Badge = ({ type }: Props) => {
   return (
     <div
-      className="rounded-full px-2 font-bold capitalize text-white"
+      className="w-fit rounded-full px-2 font-bold capitalize text-white"
       style={{ backgroundColor: getColorByType(type) }}
     >
       {type}
